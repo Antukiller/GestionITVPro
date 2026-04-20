536e18a9c893c53247f2754c1bd0ba38d7781ea5
Antukiller <antoinelpez@gmail.com>
2026-04-20 10:17:50 +0200

    Subiendo la carpeta Entity para la base de datos

---
+++ GestionITVPro/GestionITVPro/Entity/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GestionITVPro.Entity;
+
+public class AppDbContext : DbContext {
+    private readonly string _connectionString;
+
+    public AppDbContext(string connectionString) {
+        _connectionString = connectionString;
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        _connectionString = "";
+    }
+
+    public DbSet<VehiculoEntity> Vehiculos { get; set; } = null;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        if (!optionsBuilder.IsConfigured) optionsBuilder.UseSqlite(_connectionString);
+    }
+
+    public void EnsureCreated() {
+        Database.EnsureCreated();
+    }
+}
---
+++ GestionITVPro/GestionITVPro/Entity/VehiculoEntity.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionITVPro.Entity;
+
+/// <summary>
+/// Entidad de base de datos para Vehiculos.
+/// </summary>
+
+[Table("Vehiculo")]
+public class VehiculoEntity {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required] [MaxLength(7)] public string Matricula { get; set; } = string.Empty;
+
+    [Required] [MaxLength(50)] public string Modelo { get; set; } = string.Empty;
+
+    [Required] [MaxLength(50)] public string Marca { get; set; } = string.Empty;
+    
+    [Required] public int Cilindrada { get; set; }
+    
+    [Required] public int Motor { get; set; }
+
+    [Required] [MaxLength(50)] public string DniPropietario { get; set; } = string.Empty;
+
+    [Column(TypeName = "datetime2")] public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [Column(TypeName = "datetime2")] public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+    
+    public bool IsDeleted { get; set; }
+    
+    [Column(TypeName = "datetime2")] public DateTime? DeletedAt { get; set; }
+
+}
